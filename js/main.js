f96073f7d0ca5367f78fd5b3ce7e04cbb398b83e
SantiagoVilchesRomero <svilches1212@ieszaidinvergeles.org>
2023-10-17 21:40:06 +0200

    instanciar objetos

---
+++ js/main.js
@@ -0,0 +1,14 @@
+import { Tablero } from "./modules/tablero";
+import { Territorio } from "./modules/tablero";
+
+let territorio1 = new Territorio(200, 200, 20, terrrito1);
+let territorio2 = new Territorio(800, 200, 20, terrrito2);
+let territorio3 = new Territorio(200, 800, 20, terrrito3);
+let territorio4 = new Territorio(800, 800, 20, terrrito4);
+
+
+
+
+let tablero = new Tablero(1200, 1200);
+
+tablero.crearTablero();
