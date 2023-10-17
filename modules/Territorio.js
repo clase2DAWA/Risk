4dab7cff083f89a36b833d6fdf8e46ccf933e82f
agueriv <rivasguerreroariel@gmail.com>
2023-10-17 18:00:04 +0200

    Implementación de clase Territorio

---
+++ modules/Territorio.js
@@ -0,0 +1,12 @@
+class Territorio {
+  constructor(r, cx, cy) {
+    this.fill = "black";
+    this.stroke = "red";
+    this.strokeWidth = 2;
+    this.r = r;
+    this.cx = cx;
+    this.cy = cy;
+  }
+}
+
+export { Territorio };
