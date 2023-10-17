23670e4a47f0907bb38aa31c27e8205674ad3b90
SantiagoVilchesRomero <svilches1212@ieszaidinvergeles.org>
2023-10-17 21:51:54 +0200

    terminar instancias

--- js/main.js
+++ js/main.js
@@ -6,8 +6,8 @@ let territorio2 = new Territorio(800, 200, 20, terrrito2);
 let territorio3 = new Territorio(200, 800, 20, terrrito3);
 let territorio4 = new Territorio(800, 800, 20, terrrito4);
 
+let territorios = [territorio1, territorio2, territorio3, territorio4];
 
-
-let tablero = new Tablero(1200, 1200);
+let tablero = new Tablero(territorios ,1200, 1200);
 
 tablero.crearTablero();
