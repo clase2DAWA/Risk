4bcf6c0ab2ee871d17904892e0a7d478af38a6df
fransanchezp <fjsp02@gmail.com>
2023-10-17 12:00:58 +0200

    create new project

---
+++ index.js
@@ -0,0 +1,7 @@
+document.getElementById("mapid").onclick = function() {
+    console.log("pastelito");
+}
+
+document.getElementById("mapid2").onclick = function() {
+    console.log("bizcochito");
+}
