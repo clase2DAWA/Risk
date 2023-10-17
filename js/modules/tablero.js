947388a498edae8a2ca57de02d46825793cea61b
SantiagoVilchesRomero <svilches1212@ieszaidinvergeles.org>
2023-10-17 21:52:03 +0200

    terminar clase tablero

--- js/modules/tablero.js
+++ js/modules/tablero.js
@@ -11,8 +11,20 @@ class tablero {
         tablero.setAttribute("height", this.alto);
 
         let svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
-        svg.setAttribute("width",1000);
-        svg.setAttribute("heigth",1000);
+        svg.setAttribute("width", 1000);
+        svg.setAttribute("heigth", 1000);
+
+        this.territorios.forEach(function (territorio) {
+            let circulo = document.createAttributeNS("http://www.w3.org/2000/svg", "circle")
+            circulo.setAttributeNS(null, "cx", territorio.x);
+            circulo.setAttributeNS(null, "cy", territorio.y);
+            circulo.setAttributeNS(null, "r", territorio.radio);
+
+            circulo.addEventListener("click", function (){
+                console.log(territorio.id);
+            });
+            svg.appendChild(circulo);
+        });
 
         tablero.appendChild(svg);
         document.body.appendChil(tablero);
