d1ba9ebb42c05c2d057e1627f5d6a400071b986e
JavierAguirreNavarro <fjaguirre99@ieszaidinvergeles.org>
2023-10-18 13:36:32 +0200

    Tablero y territorios

---
+++ index.js
@@ -0,0 +1,3 @@
+import { Board } from "./modules/Board.js";
+let board = new Board(document.getElementById("board"));
+board.draw();
---
+++ modules/Territories.js
@@ -0,0 +1,13 @@
+class Territory {
+    constructor(name) {
+        this.name = name;
+        this.army = 0;
+    }
+    getName() {
+        return this.name;
+    }
+    setArmy() {
+        this.army = army;
+    }
+}
+export {Territory} 
