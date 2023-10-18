5507b064f689aa33b8fb042c0cbaf3f988a5ea25
JuanCarlosCarballo634 <jccarballo143@ieszaidinvergeles.org>
2023-10-18 12:01:08 +0200

    comienzo risk

---
+++ index.js
@@ -0,0 +1,4 @@
+import { Board } from "./modules/board.js";
+
+let board = new Board (document.getElementById("board"));
+board.drawBoard();
---
+++ modules/board.js
@@ -0,0 +1,45 @@
+import { Territory } from "./territory.js";
+
+class Board {
+    constructor(board) {
+        this.board = board;
+        this.territories = [];
+        let territory = new Territory ("Spain")
+        
+        this.territories.push({
+            "posx": 0,
+            "posy": 0,
+            "territory": territory
+        })
+
+        territory = new Territory ("Portugal");
+        let objeto = {
+            "posx": 0,
+            "posy": 0,
+            "territory": territory
+        }
+
+        this.territories.push(objeto);
+    }
+
+    drawBoard() {
+        let pos = 40;
+        for (let item of this.territories) {
+            
+            let circle = document.createElementNS('http://www.w3.org/2000/svg', 'circle');
+            circle.setAttribute('cx', pos);
+            circle.setAttribute('cy', 40);
+            circle.setAttribute('r', 40)
+            item.posx = 40;
+            item.posy = 40;
+            circle.addEventListener('click', function(){
+                console.log(item.territory.getName());
+            })
+            this.board.appendChild(circle);
+            pos += 100;
+        }
+    }
+}
+
+
+export {Board}
