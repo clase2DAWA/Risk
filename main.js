0e84b0c6eee9841ac94dbffd097379a78efa8c26
agueriv <rivasguerreroariel@gmail.com>
2023-10-23 15:54:59 +0200

    Adición llamada a funcón para setear territorios

--- main.js
+++ main.js
@@ -3,4 +3,6 @@ import { Board } from "./modules/board.js";
 var map = document.getElementById("board");
 var board = new Board(map);
 
-board.draw(map);
+// board.draw(map);
+board.setMap();
+board.draw2();
