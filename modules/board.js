3b54658fdfedd11125d86f81b0e0d19ff149f9c5
Tatiana <tfuentes203@ieszaidinvergeles.org>
2023-10-23 12:40:30 +0200

    Dibujar los territorios sin capturar los vecinos

--- modules/board.js
+++ modules/board.js
@@ -1,41 +1,47 @@
 import { Territory } from "./territory.js";
 
 class Board {
+  constructor(board) {
+    this.board = board;
+    this.territories = [];
 
-    constructor(board) {
-      this.board = board;
-      this.territories = [];
+    // Creamos un array de nombres de territorios
+    let territoryNames = ["Spain", "Portugal", "France", "Germany", "Italy", "Netherlands", "Belgium", "Switzerland", "Austria"];
+
+    let posx = 50;
+    let posy = 50;
+
+    for (let name of territoryNames) {
       this.territories.push({
-        "posx": 0,
-        "posy": 0,
-        "territory": new Territory("Spain")
+        "posx": posx,
+        "posy": posy,
+        "territory": new Territory(name)
       });
-      this.territories.push({
-        "posx": 0,
-        "posy": 0,
-        "territory": new Territory("Portugal")
-      });
-      console.log(this.territories[1].territory.getName());
-    }
 
-    draw() {
-      let pos = 40;
-      
-      for (let item of this.territories) {
-        let circle = document.createElementNS('http://www.w3.org/2000/svg', 'circle');
-        circle.setAttribute('cx', pos);
-        circle.setAttribute('cy', 40);
-        circle.setAttribute('r', 40);
-        circle.setAttribute('fill', 'red');
-        pos += 100;
-        this.board.appendChild(circle);
-        circle.addEventListener("click", function() {
-          console.log(item.territory.getName());
+      posx += 100;
 
-        });
+      // Si ya hemos alcanzado 3 círculos en una fila, pasamos a la siguiente fila
+      if (posx === 350) {
+        posx = 50;
+        posy += 100;
       }
     }
+  }
 
+  draw() {
+    for (let item of this.territories) {
+      let circle = document.createElementNS('http://www.w3.org/2000/svg', 'circle');
+      circle.setAttribute('cx', item.posx);
+      circle.setAttribute('cy', item.posy);
+      circle.setAttribute('r', 40);
+      circle.setAttribute('fill', 'red');
+      this.board.appendChild(circle);
+
+      circle.addEventListener("click", function () {
+        console.log(item.territory.getName());
+      });
+    }
+  }
 }
 
 export { Board };
