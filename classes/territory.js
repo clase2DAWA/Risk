461afc2422695b32aec245d93b3f032f1ebbf0c3
AdrianBernalMolina <abermol1605@ieszaidinvergeles.org>
2023-10-24 00:41:11 +0200

    copia

---
+++ classes/territory.js
@@ -0,0 +1,16 @@
+class Territory {
+    constructor(name) {
+        this.name = name;
+        this.army = 0;
+    }
+
+    getName() {
+        return this.name;
+    }
+
+    setArmy(army) {
+        this.army = army;
+    }
+}
+
+export { Territory }
--- game.js
+++ game.js
@@ -1,3 +1,5 @@
+import { Board } from "./modules/board.js";
+import { Territory } from "./modules/territory.js";
 //let circle1 = document.getElementById("circle1").onclick(console.log("hola"));
 //let circle2 = document.getElementById("circle2").onclick(console.log("hola"));
 
@@ -9,3 +11,21 @@ let circle2 = document.getElementById("circle2").onclick = function() {
     console.log("adios")
 };
 
+
+
+/*
+let territorio1 = new Territorio(200, 200, 20, "terrrito1", "red");
+let territorio2 = new Territorio(800, 200, 20, "terrrito2", "blue");
+let territorio3 = new Territorio(200, 800, 20, "terrrito3", "green");
+let territorio4 = new Territorio(800, 800, 20, "terrrito4", "yellow");
+
+   let territorios = [territorio1, territorio2, territorio3, territorio4];
+
+let tablero = new Tablero(territorios ,1200, 1200);
+
+tablero.crearTablero();
+*/
+
+let boardElement = document.getElementById("board");
+let board = new Board(boardElement);
+board.drawBoard()
