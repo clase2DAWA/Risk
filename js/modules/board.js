fad53e0fd60dde8389e1511648bd0e440b4761ac
SantiagoVilchesRomero <svilches1212@ieszaidinvergeles.org>
2023-10-24 19:50:32 +0200

    buscar vecinos

--- js/modules/board.js
+++ js/modules/board.js
@@ -30,6 +30,21 @@ class Board {
             }
         }
     }
+
+    setNeighbor(map){
+        for (let item of map.continents) {
+            for (let territory of item.territories) {
+                this.searchTerritory(territory.name).neighborTopLeft = this.searchTerritory(territory.TopLeft);
+                this.searchTerritory(territory.name).neighborTop = this.searchTerritory(territory.Top);
+                this.searchTerritory(territory.name).neighborTopRight = this.searchTerritory(territory.TopRight);
+                this.searchTerritory(territory.name).neighborRight = this.searchTerritory(territory.Right);
+                this.searchTerritory(territory.name).neighborBottomRight = this.searchTerritory(territory.BottomRight);
+                this.searchTerritory(territory.name).neighborBottom = this.searchTerritory(territory.Bottom);
+                this.searchTerritory(territory.name).neighborBottomLeft = this.searchTerritory(territory.BottomLeft);
+                this.searchTerritory(territory.name).neighborLeft = this.searchTerritory(territory.Left);
+            }
+        }
+    }
 }
 
 export { Board }
