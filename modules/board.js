b55ec36452d407ddd4310d702c63e64e0c726bf5
Tatiana <tfuentes203@ieszaidinvergeles.org>
2023-10-24 14:22:54 +0200

    Genero los continentes con sus vecinos

--- modules/board.js
+++ modules/board.js
@@ -1,61 +1,30 @@
-import { Territory } from "./territory.js";
+import { map } from "./map.js";
 
 class Board {
+
   constructor(board) {
     this.board = board;
     this.territories = [];
-
-    // Creamos un array de nombres de territorios
-    let territoryNames = ["Portugal", "Spain", "France", "Germany", "Italy", "Netherlands", "Belgium", "Switzerland", "Austria"];
-
-    let posx = 50;
-    let posy = 50;
-
-    for (let name of territoryNames) {
-      this.territories.push({
-        "posx": posx,
-        "posy": posy,
-        "territory": new Territory(name),
-        "neighbors": [] // Inicialmente, no tiene vecinos
-      });
-
-      posx += 100;
-
-      // Si ya hemos alcanzado 3 territorios en una fila, pasamos a la siguiente fila
-      if (posx === 350) {
-        posx = 50;
-        posy += 100;
+    for (let item of map.continents) {
+      for (let territory of item.territories) {
+        this.territories.push({
+          "name": territory.name
+        });
       }
-
-      // Luego, podemos definir los territorios vecinos para cada territorio
-      this.defineNeighbors();
     }
+    console.log(map);
   }
 
-  // Función para definir los territorios vecinos
-  defineNeighbors() {
-    // Define los índices de los territorios en el array
-    let numRows = 3;
-    let numCols = 3;
+  search(name) {
     for (let item of this.territories) {
-      let i = this.territories.indexOf(item); // Obtén el índice del elemento actual
-
-      let row = Math.floor(i / numRows);
-      let col = i % numCols;
-
-      // Busca los vecinos en las direcciones arriba, abajo, izquierda y derecha
-      let neighbors = [];
-      if (row > 0) neighbors.push(i - numRows); // Vecino arriba
-      if (col < numCols - 1) neighbors.push(i + 1); // Vecino derecha
-      if (row < numRows - 1) neighbors.push(i + numRows); // Vecino abajo
-      if (col > 0) neighbors.push(i - 1); // Vecino izquierda
-
-      item.neighbors = neighbors;
+      if (item.territory().getName() === name) {
+        return item.territory;
+      }
     }
   }
 
+  /*
   draw() {
-    const self = this;
     for (let item of this.territories) {
       let circle = document.createElementNS('http://www.w3.org/2000/svg', 'circle');
       circle.setAttribute('cx', item.posx);
@@ -65,10 +34,12 @@ class Board {
       this.board.appendChild(circle);
 
       circle.addEventListener("click", function () {
-        console.log(`This territory is ${item.territory.getName()} and its neighbors are : ${item.neighbors.map(index => self.territories[index].territory.getName()).join(', ')}`);
+        console.log(item.territory.getName());
       });
     }
   }
+*/
+
 }
 
 export { Board };
--- modules/territory.js
+++ modules/territory.js
@@ -1,15 +1,89 @@
-class Territory{
+class Territory {
+
     constructor(name){
         this.name = name;
         this.army = 0;
+        this.neighbors = null;
+        this.top = null;
+        this.topRight = null;
+        this.right = null;
+        this.bottomRight = null;
+        this.bottom = null;
+        this.bottomLeft = null;
+        this. left = null;
+        this.topLeft = null;
     }
-    
-    getName(){
+
+    get bottom() {
+        return this._bottom;
+    }
+
+    set bottom( value ) {
+        this._bottom = value;
+    }
+
+    get bottomLeft() {
+        return this._bottomLeft;
+    }
+
+    set bottomLeft( value ) {
+        this._bottomLeft= value;
+    }  
+
+    get bottomRight(){
+        return this._bottomRight;
+    }
+
+    set bottomRight( value ) {
+        this._bottomRight = value;
+    }
+
+    get left() {
+        return this._left;
+    }
+
+    set left( value ) {
+        this._left = value;
+    }
+
+    get right() { 
+        return this._right;
+    }
+
+    set right( value ) {
+        this._right = value;
+    }
+
+    get top() {
+        return this._top;
+    }
+
+    set top( value ) {
+        this._top = value;
+    }
+
+    get topLeft() {
+        return this._topLeft;
+    }
+
+    set topLeft( value ) {
+        this._topLeft = value;
+    }
+
+    get topRight() {
+        return this._topRight;
+    }
+
+    set topRight( value ) {
+        this._topRight = value;
+    }
+
+    getname() {
         return this.name;
     }
 
-    setArmy(army) {
-        this.army = army;
+    setArmy( army ) {
+        this.army =  army;
     }
 }
 
