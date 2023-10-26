f7e2cd8635a20fb800c4822a7cb993bfe04d1986
agueriv <rivasguerreroariel@gmail.com>
2023-10-26 16:17:04 +0200

    Implementación cambios clase y nuevo algoritmo

--- modules/board.js
+++ modules/board.js
@@ -22,41 +22,21 @@ class Board {
     // Creamos los vecinos de cada territorio
     for (let continent of map.continents) {
       for (let territory of continent.territories) {
-        // Recogemos la referencia a cada país y establecemos los vecinos de cada uno con la información del map
-        this.search(territory.name).top = this.search(territory.top);
-        this.search(territory.name).bottom = this.search(territory.bottom);
-        this.search(territory.name).left = this.search(territory.left);
-        this.search(territory.name).right = this.search(territory.right);
-        this.search(territory.name).topLeft = this.search(territory.topLeft);
-        this.search(territory.name).topRight = this.search(territory.topRight);
-        this.search(territory.name).bottomLeft = this.search(
-          territory.bottomLeft
-        );
-        this.search(territory.name).bottomRight = this.search(
-          territory.bottomRight
-        );
+        for (let neighbor of territory.neighbors) {
+          this.search(territory.name).territory.addNeighbor(neighbor);
+        }
       }
     }
-    console.log(this.territories);
-    /* Codigo antiguo
-    this.territories.push({
-      posX: 0,
-      posY: 0,
-      territory: new Territory("Spain"),
-    });
-    this.territories.push({
-      posX: 0,
-      posY: 0,
-      territory: new Territory("Portugal"),
-    });*/
   }
 
   search(name) {
-    for (const item of this.territories) {
+    let result = null;
+    for (let item of this.territories) {
       if (item.territory.getName() === name) {
-        return item.territory;
+        result = item;
       }
     }
+    return result;
   }
 
   draw() {
@@ -79,6 +59,142 @@ class Board {
       });
       this.board.appendChild(terr);
     }
+  }
+
+  // Pintar vecinos con algoritmo recursivo
+  x() {
+    let posX = 0;
+    let posY = 0;
+    let visited = [];
+
+    // Cogemos el primer elemento del array de territorios
+    let firstTerritory = this.territories.slice(0, 1).shift();
+    // Pintamos el primer elemento diciendo que si la posicion es 0 la inicializamos a 300
+    if (firstTerritory.posx === 0) {
+      posX = 300;
+      if (firstTerritory.posy === 0) {
+        posY = 300;
+      }
+    }
+    // Añadimos este territorio a la lista de visitados
+    if (!visited.includes(firstTerritory)) {
+      // Pintamos en el lienzo el primer elemento
+      var circle = document.createElementNS(
+        "http://www.w3.org/2000/svg",
+        "circle"
+      );
+
+      circle.setAttribute("cx", posX);
+      circle.setAttribute("cy", posY);
+      circle.setAttribute("r", 40);
+      circle.setAttribute("fill", "#a80083");
+      this.board.appendChild(circle);
+      firstTerritory.posx = posX;
+      firstTerritory.posy = posY;
+      circle.addEventListener("click", function () {
+        console.log(firstTerritory.territory.getName());
+      });
+      // Añadimos a la lista de visitados
+      visited.push(firstTerritory);
+    }
+    console.log(visited);
+    console.log("-----------------------------------------------");
+    console.log(firstTerritory.territory.name);
+    // Declaramos variables
+    let obtain,
+      terr,
+      neighbor,
+      neighborData,
+      neighborX,
+      neighborY,
+      offsetX,
+      offsetY;
+    // Preguntamos a este territorio si tiene vecinos
+    while (visited.length > 0) {
+      // Cogemos el ultimo elemento del array de visitados
+      terr = visited[visited.length - 1];
+      if (terr.territory.neighbors.length > 0) {
+        obtain = false;
+        for (const neighborIt of terr.territory.neighbors) {
+          if (
+            this.search(neighborIt.name) != null &&
+            this.search(neighborIt.name).posx == 0
+          ) {
+            neighbor = this.search(neighborIt.name);
+            neighborData = neighborIt;
+            console.log("PASA POR LA SELECCION DE VECINO");
+            obtain = true;
+          }
+        }
+        if (!obtain) {
+          visited.pop();
+        } else {
+          // Aqui pintaremos el vecino
+          // Establecemos el aumento de las coordenadas del padre
+          neighborX = Math.cos(neighborData.degrees * (Math.PI / 180)) * 40;
+          neighborY = Math.sin(neighborData.degrees * (Math.PI / 180)) * 40;
+          // console.log(neighborData.degrees);
+          console.log("X = " + neighborX + " Y = " + neighborY);
+          // Calculamos offsets
+          console.log(
+            terr.territory.name + ": " + terr.posx + " --- " + terr.posy
+          );
+          // Controlamos que si los grados son hacia la derecha sumanos la x y si son para la izq los restamos
+          if (neighborData.degrees > 0 && neighborData.degrees < 180) {
+            neighborX += terr.posx + 100;
+          } else if (neighborData.degrees == 0 || neighborData.degrees == 180) {
+            neighborX = terr.posx;
+          } else {
+            neighborX = terr.posx - 100;
+          }
+          if (neighborData.degrees > 90 && neighborData.degrees < 270) {
+            neighborY += terr.posy + 100;
+          } else if (
+            neighborData.degrees == 90 ||
+            neighborData.degrees == 270
+          ) {
+            neighborY = terr.posy;
+          } else {
+            neighborY = terr.posy - 100;
+          }
+
+          neighbor.posx = neighborX;
+          neighbor.posy = neighborY;
+          console.log(neighbor);
+          // Pintamos
+          console.log("Antes del evento: " + neighbor.territory.name);
+
+          this.generateCircle(
+            neighborX,
+            neighborY,
+            40,
+            "#a80083",
+            neighbor.territory.getName()
+          );
+
+          console.log("Despues del evento: " + neighbor.territory.name);
+          // Lo metemos a la lista de visitados
+          visited.push(neighbor);
+        }
+      } else {
+        visited.pop();
+      }
+    }
+  }
+
+  generateCircle(x, y, r, fill, name) {
+    let circle = document.createElementNS(
+      "http://www.w3.org/2000/svg",
+      "circle"
+    );
+    circle.setAttribute("cx", x);
+    circle.setAttribute("cy", y);
+    circle.setAttribute("r", r);
+    circle.setAttribute("fill", fill);
+    circle.addEventListener("click", function () {
+      console.log(name);
+    });
+    this.board.appendChild(circle);
   }
 }
 
