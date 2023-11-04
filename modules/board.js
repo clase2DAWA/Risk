876284bce816b2d7eeda20e419bc2b76d7a2cf42
agueriv <rivasguerreroariel@gmail.com>
2023-11-04 18:14:20 +0100

    Creación de grosor de algoritmo de vecinos

--- modules/board.js
+++ modules/board.js
@@ -7,15 +7,15 @@ class Board {
     this.territories = [];
     for (let item of map.continents) {
       for (let territory of item.territories) {
-        let element = {
+        let landDataInformation = {
           posx: 0,
           posy: 0,
           visited: false,
           territory: null,
         };
-        let elementTerritory = new Territory(territory.name, element);
-        element.territory = elementTerritory;
-        this.territories.push(element);
+        let land = new Territory(territory.name, landDataInformation);
+        landDataInformation.territory = land;
+        this.territories.push(landDataInformation);
       }
     }
 
@@ -28,6 +28,8 @@ class Board {
         }
       }
     }
+
+    console.log(this.territories);
   }
 
   search(name) {
@@ -38,24 +40,55 @@ class Board {
     }
   }
 
+  setTerritories() {
+    // Cogemos el primer territorio del array de territories
+    let land = this.territories.find((item) => item.visited == false);
+    land.posx = 300;
+    land.posy = 300;
+    land.visited = true;
+
+    let visited = [];
+    visited.push(land);
+
+    console.log(land.territory.getName());
+
+    while (visited.length) {
+      let neighborData = visited
+        .at(-1)
+        .territory.neighbors.find((item) => item.visited == false);
+
+      if (neighborData != null) {
+        let neighbor = neighborData.territory;
+        neighborData.posx = land.posx + 150;
+        neighborData.posy = land.posy;
+        neighborData.visited = true;
+
+        console.log(neighbor.getName());
+
+        visited.push(neighborData);
+
+        land = neighborData;
+      } else {
+        // Borra el ultimo de visited
+        visited.pop();
+      }
+    }
+  }
+
   draw() {
-    let pos = 40;
     for (let item of this.territories) {
       let circle = document.createElementNS(
         "http://www.w3.org/2000/svg",
         "circle"
       );
-      circle.setAttribute("cx", pos);
-      circle.setAttribute("cy", 40);
+      circle.setAttribute("cx", item.posx);
+      circle.setAttribute("cy", item.posy);
       circle.setAttribute("r", 40);
       circle.setAttribute("fill", "red");
       this.board.appendChild(circle);
-      item.posx = pos;
-      item.posy = 40;
       circle.addEventListener("click", function () {
         console.log(item.territory.getName());
       });
-      pos += 100;
     }
   }
 }
