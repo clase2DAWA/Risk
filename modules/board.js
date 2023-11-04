04771a0b66ba4a36c8ca24d7bfc186e68198d513
agueriv <rivasguerreroariel@gmail.com>
2023-11-04 17:19:40 +0100

    Creacion territorios guardando su meta información

--- modules/board.js
+++ modules/board.js
@@ -2,58 +2,62 @@ import { Territory } from "./territory.js";
 import { map } from "./map.js";
 
 class Board {
-
-    constructor( board ) {
-        this.board = board;
-        this.territories = [];
-        for (let item of map.continents) {
-            for (let territory of item.territories) {
-                this.territories.push({
-                    "posx": 0,
-                    "posy": 0,
-                    "visit": false,
-                    "territory": new Territory(territory.name)
-                });
-            }
-        }
-
-        for (let item of map.continents) {
-            for (let territory of item.territories) {
-                for (let neighbor of territory.neighbors) {
-                    this.search(territory.name).territory.addNeighbor(neighbor);
-                }
-            }
-        }
-
-        console.log(this.territories);
+  constructor(board) {
+    this.board = board;
+    this.territories = [];
+    for (let item of map.continents) {
+      for (let territory of item.territories) {
+        let element = {
+          posx: 0,
+          posy: 0,
+          visited: false,
+          territory: null,
+        };
+        let elementTerritory = new Territory(territory.name, element);
+        element.territory = elementTerritory;
+        this.territories.push(element);
+      }
     }
 
-    search( name ) {
-        for (let item of this.territories) {
-            if (item.territory.getName() === name) {
-                return item;
-            }
+    for (let item of map.continents) {
+      for (let territory of item.territories) {
+        for (let neighbor of territory.neighbors) {
+          this.search(territory.name).territory.addNeighbor(
+            this.search(neighbor.name)
+          );
         }
+      }
     }
+  }
 
-    draw() {
-        let pos = 40;
-        for (let item of this.territories) {
-           let circle = document.createElementNS('http://www.w3.org/2000/svg','circle');
-            circle.setAttribute('cx',pos);
-            circle.setAttribute('cy',40);
-            circle.setAttribute('r',40);
-            circle.setAttribute('fill','red');
-           this.board.appendChild(circle); 
-           item.posx = pos;
-           item.posy = 40 ;
-           circle.addEventListener("click", function () {
-            console.log( item.territory.getName() );
-        });
-           pos += 100;
-        }
+  search(name) {
+    for (let item of this.territories) {
+      if (item.territory.getName() === name) {
+        return item;
+      }
     }
+  }
 
+  draw() {
+    let pos = 40;
+    for (let item of this.territories) {
+      let circle = document.createElementNS(
+        "http://www.w3.org/2000/svg",
+        "circle"
+      );
+      circle.setAttribute("cx", pos);
+      circle.setAttribute("cy", 40);
+      circle.setAttribute("r", 40);
+      circle.setAttribute("fill", "red");
+      this.board.appendChild(circle);
+      item.posx = pos;
+      item.posy = 40;
+      circle.addEventListener("click", function () {
+        console.log(item.territory.getName());
+      });
+      pos += 100;
+    }
+  }
 }
 
 export { Board };
