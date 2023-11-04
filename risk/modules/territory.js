345a7422b7fbb3f907e26e7dd4666af50bdf3293
agueriv <rivasguerreroariel@gmail.com>
2023-11-04 17:08:32 +0100

    Modificación de claase territorio para jsonData

--- risk/modules/territory.js
+++ risk/modules/territory.js
@@ -1,24 +1,26 @@
 class Territory {
+  constructor(name, jsonData) {
+    this.name = name;
+    this.army = 0;
+    this.neighbors = [];
+    this.jsonData = jsonData;
+  }
 
-    constructor(name) {
-        this.name = name;
-        this.army = 0;
-        this.neighbors=[];
-    }
+  getName() {
+    return this.name;
+  }
 
-    getName() {
-        return this.name;
-    }
+  getJsonData() {
+    return this.jsonData;
+  }
 
-    setArmy(army) {
-        this.army = army;
-    }
+  setArmy(army) {
+    this.army = army;
+  }
 
-    addNeighbor(neighbor){
-        this.neighbors.push(neighbor);
-    }
-
-    
+  addNeighbor(neighbor) {
+    this.neighbors.push(neighbor);
+  }
 }
 
 export { Territory };
