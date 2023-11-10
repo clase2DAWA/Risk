ed444368ea2dd2c51411c1b7861a7a17afc4740f
Javier Aguirre Navarro <113890904+JavierAguirreNavarro@users.noreply.github.com>
2023-11-10 16:44:19 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
-import { Board } from "./modules/Board.js";
+/*let territorio1 = document.getElementById("territorio1").addEventListener("click", function () {
+    console.log("Territorio 1");
+});
+
+let territorio2 = document.getElementById("territorio2").addEventListener("click", function () {
+    console.log("Territorio 2");
+});*/
+import { Board } from "./modules/board2.js";
 let board = new Board(document.getElementById("board"));
-board.draw();
+board.asignarPosicion();
